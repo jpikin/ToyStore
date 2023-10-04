3f29bc8aa3ddf8ca40ad82083b116bba3e4d0fda
Pikin <pikin@bk.ru>
2023-10-04 13:58:52 +0500

    InputIhfo class added, Toys constructor added

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
 
+        Toys toys = new Toys(InputInfo.getID(), InputInfo.getName(), InputInfo.getFrequency());
+
     }
+
 }
---
+++ src/Toys.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Toys {
+    private int ID;
+    private String name;
+    private double frequency;
+    public Toys(int ID, String name, double frequency){
+        this.ID = ID;
+        this.name = name;
+        this.frequency = frequency;
+    }
+
+
+
+}
