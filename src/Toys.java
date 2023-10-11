df4248a91f0b768808d05397676f9f73846dc7a6
Pikin <pikin@bk.ru>
2023-10-11 15:40:15 +0500

    setfrequency method added

--- src/Toys.java
+++ src/Toys.java
@@ -1,17 +1,19 @@
-import java.util.Scanner;
 
 public class Toys {
     private String ID;
     private String name;
-    private String frequency;
+    private int frequency;
     public Toys(String [] arr) {
         this.ID = arr[0];
         this.name = arr[2];
-        this.frequency = arr[1];
+        this.frequency = Integer.parseInt(arr[1]);
     }
-public String getName(){
+    public String getName(){
         return name;
 }
+    public void setFrequency(int frequency){
+        this.frequency = frequency;
+    }
 
 
 }
